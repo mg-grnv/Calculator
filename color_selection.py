b8a69bddf7d619eee8c00f6d8cbe3f3477d54a87
nurmagomed <ngarunov@inbox.ru>
2024-02-05 16:46:14 +0300

    Сделан интерфейс окна "Выбор цвета": добавлены разметка и кнопки "Ок" и "Отмена".

--- color_selection.py
+++ color_selection.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QPushButton, QGridLayout
 
 
 class ColorSelection(QDialog):
@@ -9,3 +9,12 @@ class ColorSelection(QDialog):
     def initUI(self):
         self.setWindowTitle('Выбор цвета')
         self.setMinimumWidth(300)
+
+        grid = QGridLayout()
+        self.setLayout(grid)
+        accept_button = QPushButton('Ок')
+        accept_button.clicked.connect(self.accept)
+        grid.addWidget(accept_button, 1, 1)
+        reject_button = QPushButton('Отмена')
+        reject_button.clicked.connect(self.reject)
+        grid.addWidget(reject_button, 1, 2)
