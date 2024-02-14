50766bf6fb320481ef07379834d12c2067c33e7f
nurmagomed <ngarunov@inbox.ru>
2024-02-14 12:45:16 +0300

    Изменён функционал окна "Выбор цвета"
    Теперь при нажатии на название темы в окне "Выбор цвета" появляется картинка для предпросмотра.

--- color_selection.py
+++ color_selection.py
@@ -8,32 +8,38 @@ class ColorSelection(QDialog):
         super().__init__(*args, **kwargs)
         self.initUI()
 
+    def change_image(self):
+        current_theme = self.color_selection_combo_box.currentText()
+        current_image = self.dict_towns[current_theme] + '.jpg'
+        pixmap = QPixmap(f'Themes/{current_image}')
+        pixmap = pixmap.scaled(self.color_selection_combo_box.width(), self.color_selection_combo_box.width(), Qt.KeepAspectRatio)
+        self.picture.setPixmap(pixmap)
+
     def initUI(self):
         self.setWindowTitle('Выбор цвета')
         self.setMinimumWidth(300)
         grid = QGridLayout()
         self.setLayout(grid)
-        list_towns = [
-            'Замок',
-            'Оплот',
-            'Башня',
-            'Инферно',
-            'Некрополис',
-            'Темница',
-            'Крепость',
-            'Цитадель',
-            'Причал',
-            'Сопряжение',
-            'Фабрика'
-        ]
+        self.dict_towns = {
+            'Замок': 'Heroes_3_Castle',
+            'Оплот': 'Heroes_3_Rampart',
+            'Башня': 'Heroes_3_Tower',
+            'Инферно': 'Heroes_3_Inferno',
+            'Некрополис': 'Heroes_3_Necropolis',
+            'Темница': 'Heroes_3_Dungeon',
+            'Крепость': 'Heroes_3_Fortress',
+            'Цитадель': 'Heroes_3_Stronghold',
+            'Причал': 'Heroes_3_Cove',
+            'Сопряжение': 'Heroes_3_Conflux',
+            'Фабрика': 'Heroes_3_Factory'
+        }
         self.color_selection_combo_box = QComboBox()
-        self.color_selection_combo_box.addItems(list_towns)
+        self.color_selection_combo_box.addItems(list(self.dict_towns.keys()))
+        self.color_selection_combo_box.currentIndexChanged.connect(self.change_image)
         grid.addWidget(self.color_selection_combo_box, 0, 0, 1, 2)
         self.picture = QLabel()
+        self.change_image()
         grid.addWidget(self.picture, 1, 0, 1, 2)
-        pixmap = QPixmap('Themes/Heroes_3_Tower.jpg')
-        pixmap = pixmap.scaled(self.color_selection_combo_box.width(), self.color_selection_combo_box.width(), Qt.KeepAspectRatio)
-        self.picture.setPixmap(pixmap)
         accept_button = QPushButton('Ок')
         accept_button.clicked.connect(self.accept)
         grid.addWidget(accept_button, 2, 0)
